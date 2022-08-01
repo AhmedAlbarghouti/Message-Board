b9b91ecdcfadf5d554dcc3d8915016ccefb8b410
Ahmed Albarghouti <ahmedalbarghou@gmail.com>
2022-08-01 13:53:59 -0400

    display messages array using pug templates

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+const messages = require('../public/javascripts/messages');
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Current Messages:', messages: messages });
 });
 
 module.exports = router;
